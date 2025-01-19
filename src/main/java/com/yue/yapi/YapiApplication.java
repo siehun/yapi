af8ea753ab745003f7cf86b6accb67d4fd4ee512
siehun <126447514+siehun@users.noreply.github.com>
2025-01-19 18:03:19 +0800

    测试mysql,dubbo,redis,nacos连接

--- src/main/java/com/yue/yapi/YapiApplication.java
+++ src/main/java/com/yue/yapi/YapiApplication.java
@@ -1,9 +1,12 @@
 package com.yue.yapi;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class YapiApplication {
 
     public static void main(String[] args) {
