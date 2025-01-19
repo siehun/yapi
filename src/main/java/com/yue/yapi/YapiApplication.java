7d7da4a016de8e75d42d2bee5a2e9edc37161937
siehun <126447514+siehun@users.noreply.github.com>
2025-01-19 17:48:00 +0800

    项目脚手架初始化

---
+++ src/main/java/com/yue/yapi/YapiApplication.java
@@ -0,0 +1,13 @@
+package com.yue.yapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yue/yapi/YapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yue.yapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
