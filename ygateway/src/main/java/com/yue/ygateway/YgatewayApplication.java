8e790b482a733bb0e9b15453a0b9fe88bb57105e
siehun <126447514+siehun@users.noreply.github.com>
2025-01-20 21:22:01 +0800

    创建sdk,优化部分代码

---
+++ ygateway/src/main/java/com/yue/ygateway/YgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yue.ygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YgatewayApplication.class, args);
+    }
+
+}
---
+++ ygateway/src/test/java/com/yue/ygateway/YgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yue.ygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
