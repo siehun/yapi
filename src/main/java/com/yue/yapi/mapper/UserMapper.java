fbe8b46d78ee5964cefda6ec6d64de84be738b52
siehun <126447514+siehun@users.noreply.github.com>
2025-01-23 20:43:14 +0800

    更新用户的基本功能

---
+++ src/main/java/com/yue/yapi/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.yue.yapi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
