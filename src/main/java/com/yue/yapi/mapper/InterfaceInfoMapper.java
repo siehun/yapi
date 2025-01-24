8ee861ce8a2dda2be20d7e66a4cdd706cdaf26a8
siehun <126447514+siehun@users.noreply.github.com>
2025-01-24 15:14:15 +0800

    接口增删改查，基本功能的实现，

---
+++ src/main/java/com/yue/yapi/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,14 @@
+package com.yue.yapi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 接口信息 Mapper
+ *
+ */
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
---
+++ src/main/java/com/yue/yapi/mapper/PostMapper.java
@@ -0,0 +1,16 @@
+package com.yue.yapi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.project.model.entity.Post;
+
+/**
+ * 帖子信息 Mapper
+ *
+ */
+public interface PostMapper extends BaseMapper<Post> {
+
+}
+
+
+
+
